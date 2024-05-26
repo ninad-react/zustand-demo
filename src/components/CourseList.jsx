b02b3491b805e9e6fd6f88c4c31a2eb497bc50e4
ninad-react <157863834+ninad-react@users.noreply.github.com>
2024-05-26 15:03:40 +0530

    zustand demo  app

---
+++ src/components/CourseList.jsx
@@ -0,0 +1,55 @@
+import React, { Fragment } from 'react'
+import useCourseStore from '../app/courseStore'
+
+const CourseList = () => {
+
+    const {courses, removeCourse, toggleCourseStatus} = useCourseStore(
+        (state) => ({
+            courses: state.courses,
+            removeCourse: state.removeCourse,
+            toggleCourseStatus: state.toggleCourseStatus
+        })
+    )
+
+  return (
+    <>
+        <ul>
+            {courses.map((course, i) => {
+                return (
+                    <Fragment key={i}>
+                        <li 
+                            className={`course-item`}
+                            style={{
+                                backgroundColor: course.completed ? '#00FF0044' : 'white'
+                            }}
+                        >
+                            <span className='course-item-col-1'>
+                                <input 
+                                checked={course.completed}
+                                type='checkbox'
+                                onChange={(e) => {
+                                    toggleCourseStatus(course.id)
+                                }}
+                                >
+                                </input>
+                            </span>
+                            <span>
+                                {course?.title}
+                            </span>
+                            <button
+                                onClick={(e) => {
+                                    removeCourse(course.id)
+                                }}
+                                className='delete btn'>
+                                    Delete
+                            </button>
+                        </li>
+                    </Fragment>
+                )
+            })}
+        </ul>
+    </>
+  )
+}
+
+export default CourseList
